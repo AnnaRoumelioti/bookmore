3b40e27308e715680ae4eb8471048063223257e1
AnnaRoumelioti <mrs.adelaida@gmail.com>
2026-08-11 09:16:29 +0300

    Improved CTAs fn in the Book details. Added pop-up for the successful purchase

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -36,6 +36,8 @@ if (!response.ok) {
 
 
         if (data.favorited) {
+    
+            if (button && button.classList.contains("card-icons")) {
 
             button.innerHTML = `
                 <svg
@@ -51,6 +53,7 @@ if (!response.ok) {
 
                 </svg>
             `;
+            }
 
             showFavoriteMessage(
                 "Book is added to Favorites",
@@ -58,6 +61,8 @@ if (!response.ok) {
             );
 
         } else {
+
+            if (button && button.classList.contains("card-icons")) {
 
             button.innerHTML = `
                <svg
@@ -72,6 +77,7 @@ if (!response.ok) {
                 d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.837-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143 7.89 1.2 7.946 1.29 8 1.386a.87.87 0 0 1 .176-.243C12.72-3.042 23.333 4.868 8 15"/>
         </svg>
             `;
+            }
 
             showFavoriteMessage(
                 "Book is removed from Favorites",
